bc6d15f1cc79efe724eb15ac78253108b646e518
Kris Schmidt <kris.schmidt7@gmail.com>
2016-04-05 23:45:33 -0400

    fix test imports

--- tests/containers/CoreLayout/CoreLayout.spec.js
+++ tests/containers/CoreLayout/CoreLayout.spec.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import TestUtils from 'react-addons-test-utils'
-import CoreLayout from 'layouts/CoreLayout/CoreLayout'
+import CoreLayout from 'containers'
 
 function shallowRender (component) {
   const renderer = TestUtils.createRenderer()
